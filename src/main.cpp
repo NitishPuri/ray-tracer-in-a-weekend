b744cba0e430756ce9881244ca606b95734c94ec
Nitish Puri <npuri1903@gmail.com>
2023-01-30 00:18:29 +0530

    Linear gradient using rays.

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,31 @@ color ray_color(const ray& r) {
 int main() {
 
     // Image
-
-    const int image_width = 600;
-    const int image_height = 500;
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 1000;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
     Image image(image_width, image_height);
 
-    // Render
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
 
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_origin = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length); 
+
+    // Render    
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height-1; j >= 0; --j) {
         std::cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i) / (image_width-1), double(j) / (image_height-1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_origin + u * horizontal + v * vertical - origin);            
+            color pixel_color = ray_color(r);
 
             image.set(i, j, pixel_color);
         }
