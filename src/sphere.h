6441ee43b966b5957f6a0c55ee89f48120d755e6
Nitish Puri <npuri1903@gmail.com>
2023-01-30 00:55:15 +0530

    Add sphere class that implements hittable interface.

---
+++ src/sphere.h
@@ -0,0 +1,42 @@
+#pragma once 
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+    public:
+        sphere() {}
+        sphere(point3 c, double r) : center{c}, radius{r} {}
+
+        bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+    public:
+        point3 center;
+        float radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius*radius;
+
+    auto discriminant = half_b*half_b - a*c;
+    if(discriminant < 0) {
+        return false;
+    }         
+    auto sqrtd = sqrt(discriminant);
+
+    // Find the nearest root that lies in the acceptable range.
+    auto root = (-half_b - sqrtd) / a;
+    if(root < t_min || root > t_max) {
+        root = (-half_b + sqrtd) / a;
+        if(root < t_min || t_max < root)
+        return false;
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    rec.normal = (rec.p - center) / radius;
+    
+    return true;    
+}
