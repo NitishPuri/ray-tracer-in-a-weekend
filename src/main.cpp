1d67e14bb1e28179bfc7d8beb43654c119fec9d0
Nitish Puri <npuri1903@gmail.com>
2023-01-30 23:40:48 +0530

    Add stopping condition to the recursive ray bounce.

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,15 @@
 #include <string>
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
+
+    // if we've exceeded the ray bounce limit, no more light is gathered
+    if(depth < 0) return color(0, 0, 0);
+
     if(world.hit(r, 0, infinity, rec)) {
         point3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world);
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
@@ -28,6 +32,7 @@ int main() {
     const int image_width = 1000;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
     Image image(image_width, image_height);
 
     // World
@@ -49,7 +54,7 @@ int main() {
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
 
             image.set(i, j, pixel_color / samples_per_pixel);
