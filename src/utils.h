bcc67b2ebf11e9db273c2b535ab1e5a456ecfe28
Nitish Puri <npuri1903@gmail.com>
2023-01-30 22:52:41 +0530

    Add random number utils.

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -16,6 +17,15 @@ const double pi = 3.1415926535897932385;
 // Utility functions
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+// Returns a random number in range [0, 1)
+inline double random_double() {
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    return min + (max - min) * random_double();
 }
 
 // Common headers
