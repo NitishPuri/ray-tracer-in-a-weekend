72be964daf46c631a9fbb28f5f56d0c1e2d7bd70
Nitish Puri <npuri1903@gmail.com>
2023-01-30 23:57:44 +0530

    Changing image size to 500 pixels.

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ int main() {
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1000;
+    const int image_width = 500;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
     const int max_depth = 50;
