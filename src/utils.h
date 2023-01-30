1170ed7a93b495c2f33a1a007e0c3edd6d1bd8e9
Nitish Puri <npuri1903@gmail.com>
2023-01-30 23:14:53 +0530

    add clamp method.

--- src/utils.h
+++ src/utils.h
@@ -28,6 +28,12 @@ inline double random_double(double min, double max) {
     return min + (max - min) * random_double();
 }
 
+inline double clamp(double x, double min, double max) {
+    if(x < min) return min;
+    if(x > max) return max;
+    return x;
+}
+
 // Common headers
 #include "ray.h"
 #include "vec3.h"
