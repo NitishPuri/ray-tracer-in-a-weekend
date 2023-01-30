ad1139fc0ba83d49bbd91dde3b137f2d83f13db1
Nitish Puri <npuri1903@gmail.com>
2023-01-30 23:25:33 +0530

    add random vec methods.

--- src/vec3.h
+++ src/vec3.h
@@ -44,6 +44,14 @@ public:
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    inline static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    inline static vec3 random(double min, double max) {
+        return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
+    }
+
 public:
     double e[3];
 };
