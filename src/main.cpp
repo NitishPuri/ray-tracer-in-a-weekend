ade13599a4467104115c0ae30cace4adec18052a
Nitish Puri <npuri1903@gmail.com>
2023-01-30 22:46:39 +0530

    Use updated classes in main.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,22 @@
 
-#include "image.h"
-#include "ray.h"
-#include "vec3.h"
+#include "utils.h"
 #include "color.h"
+#include "image.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <string>
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    if(discriminant < 0) {
-        return -1.0;
-    }         
-    return (-half_b - sqrt(discriminant)) / a;
-}
+color ray_color(const ray& r, const hittable& world) {
 
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if(t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return  0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    hit_record rec;
+    if(world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -38,6 +27,11 @@ int main() {
     const int image_width = 1000;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     Image image(image_width, image_height);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
@@ -58,12 +52,12 @@ int main() {
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r(origin, lower_left_origin + u * horizontal + v * vertical - origin);            
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
 
             image.set(i, j, pixel_color);
         }
     }
 
-    image.write("sphereNormals.jpg");
+    image.write("../../results/sphereNormals3.jpg");
     
 }
