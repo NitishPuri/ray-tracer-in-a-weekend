b701391c22b975a8e3adf306f2ab34882e30cb3a
Nitish Puri <npuri1903@gmail.com>
2023-01-30 23:28:30 +0530

    add method to generate random direction in unit sphere.

--- src/vec3.h
+++ src/vec3.h
@@ -105,3 +105,11 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if(p.length_squared() >= 1) continue;
+        return p;
+    }
+}
