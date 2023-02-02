275cccc1921f198aa19b873e8d2884156ea7b6cc
Nitish Puri <npuri1903@gmail.com>
2023-02-03 00:56:53 +0530

    Add results with holllow glass sphere, add camera vfov option.

--- src/main.cpp
+++ src/main.cpp
@@ -42,22 +42,24 @@ int main() {
     Image image(image_width, image_height);
 
     // World
+    auto R = cos(pi/4);
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color{0.8, 0.8, 0.0});
-    auto material_center = make_shared<lambertian>(color{0.1, 0.2, 0.5});
+    // auto material_ground = make_shared<lambertian>(color{0.8, 0.8, 0.0});
+    // auto material_center = make_shared<lambertian>(color{0.1, 0.2, 0.5});
     // auto material_center = make_shared<dielectric>(1.5);
-    // auto material_left = make_shared<metal>(color{0.8, 0.8, 0.0}, 0.3);
-    auto material_left = make_shared<dielectric>(1.5);
+    auto material_left = make_shared<metal>(color{0.8, 0.8, 0.0}, 0.3);
+    // auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color{0.8, 0.6, 0.2}, 1);
 
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    // world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100, material_ground));
+    // world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    // world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-R, 0.0, -1.0), R, material_left));
+    world.add(make_shared<sphere>(point3(R, 0.0, -1.0), R, material_right));
 
     // Camera
-    camera cam;
+    camera cam(90.0, aspect_ratio);
 
     // Render    
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -88,7 +90,7 @@ int main() {
     std::cout << "Image generated in " << duration << "seconds\n";
 
     // image.write("../../results/sphereTrueLambertian.jpg");
-    std::string result_path("results/sphereMaterialGlass2.jpg");
+    std::string result_path("results/sphereCameraAspect.jpg");
     std::cout << "\nWriting result to :: " << std::filesystem::current_path().append(result_path) << std::endl;
     if(image.write(result_path) != 0) {
         std::cout << "Success!";
