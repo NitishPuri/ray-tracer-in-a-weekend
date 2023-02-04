ffab09ec78f65fb69848756ddcc37e6d8194da86
Nitish Puri <npuri1903@gmail.com>
2023-02-05 00:03:03 +0530

    Add diffuse light material.

--- src/material.h
+++ src/material.h
@@ -8,6 +8,10 @@ public:
     virtual bool scatter(
         const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
     ) const = 0;
+
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
 };
 
 class lambertian : public material {
@@ -87,4 +91,23 @@ private:
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
 
+};
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    bool scatter(
+        const ray& ray_in, const hit_record& rec, color& attenuation, ray& scattered
+    ) const override {
+        return false;
+    }
+
+    color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+private:
+    shared_ptr<texture> emit;
 };
