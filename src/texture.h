93aeed6e4b0c43c5148fc71142fff2642bc51a7a
Nitish Puri <npuri1903@gmail.com>
2023-02-04 18:39:44 +0530

    Add checker texture class.

--- src/texture.h
+++ src/texture.h
@@ -22,3 +22,24 @@ public:
 private:
     color color_value;
 };
+
+class checker_texture : public texture {
+public:
+    checker_texture() {}
+    checker_texture(shared_ptr<texture> _even, shared_ptr<texture> _odd)
+        : even(_even), odd(_odd) {}
+    checker_texture(color c1, color c2) 
+        : even(make_shared<solid_color>(c1)), odd(make_shared<solid_color>(c2)) {}
+
+    color value(double u, double v, const point3& p) const override {
+        auto sines = sin(10 * p.x()) * sin(10 * p.y()) * sin(10 * p.z());
+        if (sines < 0)
+            return odd->value(u, v, p);
+        else
+            return even->value(u, v, p);
+    }
+
+private:
+    shared_ptr<texture> odd;
+    shared_ptr<texture> even;
+};
