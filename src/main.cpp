24bc2db2a36d907871aa1d945e73e6561e36fecc
Nitish Puri <npuri1903@gmail.com>
2023-02-05 00:10:00 +0530

    add emit color contribution to ray color.

--- src/main.cpp
+++ src/main.cpp
@@ -18,23 +18,28 @@
 #define ROOT "."
 #endif // !ROOT
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
     hit_record rec;
 
     // if we've exceeded the ray bounce limit, no more light is gathered
     if(depth < 0) return color(0, 0, 0);
 
-    if(world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
-        if(rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth - 1);
-        return color(0, 0, 0);
-    }
+    // If the ray hits nothing, return the background color.
+    if (!world.hit(r, 0.001, infinity, rec))
+        return background;
 
-    vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+    if(!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        return emitted;
+
+    return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
+
+    //vec3 unit_direction = unit_vector(r.direction());
+    //auto t = 0.5 * (unit_direction.y() + 1.0);
+    //return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 hittable_list random_scene() {
@@ -134,10 +139,12 @@ int main() {
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
+    color background(0, 0, 0);
 
     switch (4) {
     case 1:
         world = random_scene();
+        background = color(0.7, 0.8, 1.0);
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
@@ -147,18 +154,21 @@ int main() {
     default:
     case 2:
         world = two_spheres();
+        background = color(0.7, 0.8, 1.0);
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         break;
     case 3:
         world = two_perlin_spheres();
+        background = color(0.7, 0.8, 1.0);
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
         break;
     case 4:
         world = earth();
+        background = color(0.7, 0.8, 1.0);
         lookfrom = point3(13, 2, 3);
         lookat = point3(0, 0, 0);
         vfov = 20.0;
@@ -189,7 +199,7 @@ int main() {
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 //pixel_color += ray_color(r, world, max_depth);
-                pixel_color += ray_color(r, world_bvh, max_depth);
+                pixel_color += ray_color(r, background, world_bvh, max_depth);
             }
 
              // Divide the color by the number of samples and gamma-correct for gamma=2.0.
