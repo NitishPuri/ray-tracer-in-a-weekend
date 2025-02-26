082259e43d5dc817b7e0181637bf362cbe36ce0f
Nitish Puri <npuri1903@gmail.com>
2025-02-26 13:11:39 +0530

    utils

--- src/integrate_x2.cpp
+++ src/integrate_x2.cpp
@@ -2,16 +2,7 @@
 #include <iostream>
 #include <random>
 
-// inline double random_double() { return rand() / (RAND_MAX + 1.0); }
-inline double random_double() {
-  static thread_local std::mt19937 generator;
-  std::uniform_real_distribution<double> distribution(0.0, 1.0);
-  return distribution(generator);
-}
-
-inline double random_double(double min, double max) {
-  return min + (max - min) * random_double();
-}
+#include "utils.h"
 
 int main() {
   int a = 0;
--- src/utils.h
+++ src/utils.h
@@ -6,8 +6,8 @@
 #include <memory>
 
 // Using
-using std::shared_ptr;
 using std::make_shared;
+using std::shared_ptr;
 using std::sqrt;
 
 // Constants
@@ -16,31 +16,23 @@ const double pi = 3.1415926535897932385;
 
 // Utility functions
 inline double degrees_to_radians(double degrees) {
-    return degrees * pi / 180.0;
+  return degrees * pi / 180.0;
 }
 
 // Returns a random number in range [0, 1)
-inline double random_double() {
-    return rand() / (RAND_MAX + 1.0);
-}
+inline double random_double() { return rand() / (RAND_MAX + 1.0); }
 
 inline double random_double(double min, double max) {
-    return min + (max - min) * random_double();
+  return min + (max - min) * random_double();
 }
 
 inline int random_int(int min, int max) {
-    // Returns a random integer in [min,max].
-    return static_cast<int>(random_double(min, max + 1));
+  // Returns a random integer in [min,max].
+  return static_cast<int>(random_double(min, max + 1));
 }
 
 inline double clamp(double x, double min, double max) {
-    if(x < min) return min;
-    if(x > max) return max;
-    return x;
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
 }
-
-// Common headers
-#include "ray.h"
-#include "hittable.h"
-#include "vec3.h"
-#include "texture.h"
