b4cd0b6df14aa9f371cd6f5a5fced9a6e33eeda1
Nitish Puri <npuri1903@gmail.com>
2025-02-26 11:18:04 +0530

    estimate pi stratified

--- src/estimate_pi.cpp
+++ src/estimate_pi.cpp
@@ -17,21 +17,25 @@ int main() {
   std::cout << std::fixed << std::setprecision(12);
 
   int inside_circle = 0;
-  int runs = 0;
+  int inside_circle_stratified = 0;
+  int sqrt_N = 10000;
 
-  while (true) {
-    runs++;
-    auto x = random_double(-1, 1);
-    auto y = random_double(-1, 1);
-    if (x * x + y * y <= 1) {
-      ++inside_circle;
-    }
+  for (int i = 0; i < sqrt_N; i++) {
+    for (int j = 0; j < sqrt_N; j++) {
+      auto x = random_double(-1, 1);
+      auto y = random_double(-1, 1);
+      if (x * x + y * y < 1) inside_circle++;
 
-    if (runs % 1000000 == 0) {
-      std::cout << "Pi is roughly " << 4.0 * inside_circle / runs << std::endl;
+      x = 2 * ((i + random_double()) / sqrt_N) - 1;
+      y = 2 * ((j + random_double()) / sqrt_N) - 1;
+      if (x * x + y * y < 1) inside_circle_stratified++;
     }
   }
 
-  // std::cout << "Pi is roughly " << 4.0 * inside_circle / N << std::endl;
+  std::cout << "Regular    Estimate of Pi = "
+            << (4.0 * inside_circle) / (sqrt_N * sqrt_N) << '\n'
+            << "Stratified Estimate of Pi = "
+            << (4.0 * inside_circle_stratified) / (sqrt_N * sqrt_N) << '\n';
+
   return 0;
 }
